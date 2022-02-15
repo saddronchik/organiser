e120c10c1af7b1bdf8a8188dfbb88f2e02e6f8ce
andrew <andrenik449@gmail.com>
2022-02-15 10:49:36 +0300

    update files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        \App\Models\Department::factory(10)->create();
-//         \App\Models\User::factory(10)->create();
-//        \App\Models\Assignment::factory(50)->create();
+    //    \App\Models\Department::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
+    //    \App\Models\Assignment::factory(50)->create();
     }
 }
