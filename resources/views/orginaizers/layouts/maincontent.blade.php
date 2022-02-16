e0bfeaa39989213a7908977f9e4520ac50e8c3ab
andrew <andrenik449@gmail.com>
2022-02-16 10:08:52 +0300

    fix bags on main page

--- resources/views/orginaizers/layouts/maincontent.blade.php
+++ resources/views/orginaizers/layouts/maincontent.blade.php
@@ -4,12 +4,12 @@
     <title>Органайзер</title>
 
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.bundle.js') }}"></script>
     <script src="{{ asset('js/moment.min.js') }}"></script>
     <script src="{{ asset('js/todayEvent.js') }}"></script>
-    <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
-    <script src="{{ asset('js/ru.js')}}"></script>
     <script src="{{ asset('js/fullcalendar.js') }}"></script>
+    <script src="{{ asset('js/ru.js')}}"></script>
 
     <link rel="stylesheet" href="css\fullcalendar.css" />
     <link rel="stylesheet" href="css\fullcalendar-custom.css" />
